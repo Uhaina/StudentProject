342ee62084ca951792ed01ff0f6d740665116733
Uhaina <hello@davidvelten.fr>
2018-03-24 01:30:35 +0100

    initial commit

--- src/pages/About/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class extends React.Component {
-  render = () => (
-    <div>
-      About page
-    </div>
-  )
-}
--- src/pages/Home/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default class extends React.Component {
-  render = () => (
-    <div className="home">Home page</div>
-  )
-}
